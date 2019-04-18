a7e076956a0d3e6aaf8720aef06dcfb980251e23
VirtanAlinaElena <48881732+VirtanAlinaElena@users.noreply.github.com>
2019-04-19 00:20:24 +0300

    Create tema2SD.h

---
+++ tema2SD.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define BUFMAX 10000
+
+// typedef struct list {
+// 	char *value;
+// 	struct list* next;
+// }*List;
+
+typedef struct tree 
+{
+	// id-ul nodului
+	int value;
+	
+	// id-ul parintelui
+	int parentValue;
+	
+	// legatura catre nodul parinte - inca nu stiu daca am nevoie
+	struct tree *parent;
+
+	// copiii nodului
+	struct tree *child;
+	struct tree *sibling;
+	
+	// numarul de adrese rezolvabila
+	int nrAddress;
+	
+	// vectorul de adrese
+	char **address;
+} *Tree;
+
+
+Tree initTree(int value, int parentValue, int nrAddress, char** address);
+int noOfKidsTree(Tree tree);
+Tree insertChildTree(Tree tree, int value, int parentValue, int nrAddress, 
+							char** address);
+Tree deleteChildTree(Tree tree, int value);
+Tree findNode(Tree tree, int value);
+
+// List initList(char* value);
+// List addLastList(List l, char* value);
+// List addItemList(List l, char* address, char* value);
+// List addFirstList(List l, char* value);
